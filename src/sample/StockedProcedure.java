3d0992afe23c44f8588810200c15e09e839ae32c
amine <amine-katcho@hotmail.com>
2016-10-22 22:45:42 +0200

    bug fix

--- src/sample/StockedProcedure.java
+++ src/sample/StockedProcedure.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.scene.control.ComboBox;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 /**
  * Created by amine on 10/3/2016.
@@ -11,10 +14,12 @@ public class StockedProcedure {
 
     public static Connection c;
     public static Statement stmt;
+    static DateFormat df = new SimpleDateFormat("dd/MM/yyyy ");
+    static java.util.Date dateobj = new java.util.Date();
 
-    public static void nomprenom(ComboBox<String> kk){
+    public static void nomprenom(ComboBox<String> kk) {
 
-         c = null;
+        c = null;
         stmt = null;
         try {
             String url = "jdbc:sqlite:C:/Users/amine/Documents/mobilis.sqlite "; //C:\Users\amine\Documents\mobilis.sqlite
@@ -22,11 +27,11 @@ public class StockedProcedure {
             c = DriverManager.getConnection(url);
 
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT id,nom,prenom FROM employes ;" );
+            ResultSet rs = stmt.executeQuery("SELECT id,nom,prenom FROM employes ;");
 
-            while ( rs.next() ) {
+            while (rs.next()) {
 
-                String  name = rs.getInt("id")+"  "+rs.getString("nom")+" "+rs.getString("prenom");
+                String name = rs.getInt("id") + "  " + rs.getString("nom") + " " + rs.getString("prenom");
                 kk.getItems().add(name);
             }
             rs.close();
@@ -41,7 +46,7 @@ public class StockedProcedure {
         new ComboBoxAutoComplete<>(kk);
     }
 
-    public static void addcongé(String debut ,String fini , String lieu ,int id_intérime  ,int id_employé){
+    public static void addcongé(String debut, String fini, String lieu, int id_intérime, int id_employé) {
         c = null;
         stmt = null;
         try {
@@ -52,13 +57,13 @@ public class StockedProcedure {
 
             PreparedStatement prep = c.prepareStatement(
                     "INSERT INTO info_congé (id_employé,debut,fin, lieu_de_juissance, id_intérimaire)\n" +
-                    "VALUES (?,?,?,?,?);");
+                            "VALUES (?,?,?,?,?);");
 
-            prep.setInt(1,id_employé);
-            prep.setString(2,debut);
-            prep.setString(3,fini);
-            prep.setString(4,lieu);
-            prep.setInt(5,id_intérime);
+            prep.setInt(1, id_employé);
+            prep.setString(2, debut);
+            prep.setString(3, fini);
+            prep.setString(4, lieu);
+            prep.setInt(5, id_intérime);
             prep.execute();
             stmt.close();
             c.close();
@@ -71,8 +76,7 @@ public class StockedProcedure {
 
     }
 
-
-    public static void add_abs_sort(int id_employé, int typee, String desire, String motif, String datee) {
+    public static void add_abs_sort(int id_employé, String nom, String prenom, int typee, String desire, String motif, String datee) {
         c = null;
         stmt = null;
         try {
@@ -82,15 +86,22 @@ public class StockedProcedure {
             stmt = c.createStatement();
 
 
+            ResultSet rs = stmt.executeQuery("SELECT * FROM employes WHERE id=" + id_employé + ";");
+
+            String fonction = rs.getString(4);
+            String date_recru = rs.getString(5);
+            String struct = rs.getString(8);
+            replace.replace_info(2, nom, prenom, date_recru, fonction, struct, datee, desire, motif);
+
             PreparedStatement prep = c.prepareStatement(
                     "INSERT INTO info_sortie (id_employe,type,Desire,motif,date_sortie)\n" +
                             "VALUES (?,?,?,?,?);");
 
-            prep.setInt(1,id_employé);
-            prep.setInt(2,typee);
+            prep.setInt(1, id_employé);
+            prep.setInt(2, typee);
             prep.setString(3, desire);
             prep.setString(4, motif);
-            prep.setString(5, datee);
+            prep.setString(5, df.format(dateobj));
             prep.execute();
             stmt.close();
             c.close();
@@ -101,7 +112,6 @@ public class StockedProcedure {
             e.printStackTrace();
         }
     }
-
 
     public static void info_employé() {
         c = null;
@@ -141,6 +151,55 @@ public class StockedProcedure {
 
     }
 
+    public static void add_employé(String nom, String prenom, String fonction, String date_recru, String struct) {
+        c = null;
+        stmt = null;
+        try {
+            String url = "jdbc:sqlite:C:/Users/amine/Documents/mobilis.sqlite "; //C:\Users\amine\Documents\mobilis.sqlite
+            // create a connection to the database
+            c = DriverManager.getConnection(url);
+            stmt = c.createStatement();
 
+            PreparedStatement prep = null;
+
+            prep = c.prepareStatement(
+                    "INSERT INTO employes (nom,prenom, fonction,date_recrutement,structure_actuelle)\n" +
+                            "VALUES (?,?,?,?,?);");
+            prep.setString(1, nom);
+            prep.setString(2, prenom);
+            prep.setString(3, fonction);
+            prep.setString(4, date_recru);
+            prep.setString(5, struct);
+            prep.execute();
+            stmt.close();
+            c.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete_emploi(int id) {
+        c = null;
+        stmt = null;
+        try {
+            String url = "jdbc:sqlite:C:/Users/amine/Documents/mobilis.sqlite "; //C:\Users\amine\Documents\mobilis.sqlite
+            // create a connection to the database
+            c = DriverManager.getConnection(url);
+            stmt = c.createStatement();
+
+            PreparedStatement prep = null;
+            prep = c.prepareStatement(
+                    "DELETE FROM employes WHERE id=" + id + ";");
+
+            prep.execute();
+            stmt.close();
+            c.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
+
